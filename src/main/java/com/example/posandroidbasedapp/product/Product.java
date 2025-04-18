166d1d069a876184d8d47b249dcd5982f418cd73
rielyta <desi@students.usu.ac.id>
2025-04-18 23:41:53 +0700

    variabel product

---
+++ src/main/java/com/example/posandroidbasedapp/product/Product.java
@@ -0,0 +1,14 @@
+package com.example.posandroidbasedapp.product;
+
+public class Product {
+    public String id;
+    public String name;
+    public double price;
+
+    public Product(String id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+
+    }
+}
